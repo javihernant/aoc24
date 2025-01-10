7eaf0661db3bad977bc4a4c54c7b96f2506f961c
Javier Hernandez <javihernant@gmail.com>
2025-01-10 19:47:37 +0100

    d7b,d8

---
+++ d8b.py
@@ -0,0 +1,47 @@
+import sys
+
+def solve(antenas, dims):
+    w, h = dims
+    nodes = set()
+    for ls in antenas.values():
+        for i,(ai, aj) in enumerate(ls):
+            for j,(bi,bj) in enumerate(ls):
+                if i != j:
+                    c = 1
+                    ti = ai -c *(bi-ai)
+                    tj = aj -c *(bj-aj)
+                    nodes.add((bi,bj))
+                    while (ti >= 0 and ti < h and tj >= 0 and tj < w):
+                        nodes.add((ti,tj))
+                        c+=1
+                        ti = ai -c *(bi-ai)
+                        tj = aj -c *(bj-aj)
+    return len(nodes)
+
+def main(*args):
+    if len(args) != 1:
+        print("Enter path to file")
+        return
+    antenas = dict()
+    with open(args[0]) as fp:
+        data = fp.read()
+        data = data.splitlines()
+        h = len(data)
+        w = len(data[0])
+        for i,line in enumerate(data):
+            for j, ch in enumerate(line):
+                if ch != '.':
+                    if ch not in antenas:
+                        antenas[ch] = [(i,j)]
+                    else:
+                        antenas[ch].append((i,j))
+        sol = solve(antenas,(w,h))
+        print("Solution:", sol)
+
+def test():
+    op_rec([2,3], 0, 3, 1)
+    
+
+if __name__ == '__main__':
+    #test()
+    main(*sys.argv[1:])
